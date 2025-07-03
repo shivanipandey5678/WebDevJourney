16d7686228d0eef53519854a85db48c3c8badc27
shivanipandey5678 <shivanipandey0107@gmail.com>
2025-07-03 22:08:31 +0530

    practice

---
+++ 1-07-2025/question11/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState ,React} from 'react';
+import './App.css';
+
+const App = () => {
+   const [task,setTask]=useState('');
+   const [taskList,setTaskList]=useState([]);
+
+   return (
+    <>
+       <input value={}/>
+      
+    </>
+   )
+
+}
